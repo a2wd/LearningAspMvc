2cc86a63b03d61d6308d15a1de02e7c22de7121f
a2wd <github@a2wd.com>
2016-03-03 14:46:35 +0000

    Lesson 1: Introduction to ASP.NET MVC 4 Lesson completed

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Location = "3chillies, Gilette Way";
+            model.Name = "CSI Whitley";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
