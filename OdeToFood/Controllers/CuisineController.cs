3f9dd41e70082cb2e45e5c391a62ac05df3762c7
a2wd <github@a2wd.com>
2016-03-03 15:42:14 +0000

    Lesson 2: Controllers, redirect to controller

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -17,7 +17,7 @@ namespace OdeToFood.Controllers
         public ActionResult Search(string name)
         {
             var message = Server.HtmlEncode(name);
-            return RedirectPermanent("http://www.google.com");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
